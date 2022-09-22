0ed1f32bd212546db09e3718b7d109acf1a2330e
Saksykov <saksykovaa@mail.ru>
2022-09-22 10:40:06 +0300

    Fix bag

--- tests/tests_UI/test_UI_timetta/test_HomePage.py
+++ tests/tests_UI/test_UI_timetta/test_HomePage.py
@@ -6,7 +6,6 @@ class TestNavbar:
     """
     In class we testing Home Page Navbar objects
     """
-    @pytest.mark.parametrize()
     def test_navbar(self, chr_driver):
         navbar = HomepageNavbar(chr_driver)
         brand = navbar.navbar_brand
