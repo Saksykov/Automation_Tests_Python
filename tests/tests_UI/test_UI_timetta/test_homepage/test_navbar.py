8e7fbf8bd9fbd7c557c0de43808d1229f04c54b9
Saksykov <saksykovaa@mail.ru>
2022-09-26 19:16:28 +0300

    Update UI tests

--- tests/tests_UI/test_UI_timetta/test_homepage/test_navbar.py
+++ tests/tests_UI/test_UI_timetta/test_homepage/test_navbar.py
@@ -1,7 +1,18 @@
+import pytest
 from tests.tests_UI.pom.homepage_navbar import HomepageNavbar
 
 
-def test_navbar_brand(chr_driver):
+@pytest.fixture(autouse='False')
+def navbar(chr_driver):
     navbar = HomepageNavbar(chr_driver)
+    return navbar
+
+
+def test_navbar_brand(navbar):
     brand = navbar.navbar_brand
     brand.click()
+
+
+def test_nav_dropdown_items(navbar):
+    dropdown_items = navbar.nav_dropdown_items
+    print(dropdown_items)
